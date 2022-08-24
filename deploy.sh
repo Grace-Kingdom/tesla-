cc514c2a281b61fe1a02e234aeb17ab09a828438
simon <acha_grace@yahoo.com>
2022-08-24 17:30:06 +0000

    conflict 3 resolved

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@ deploy python apps
 deploy in the dev env
 deployment5 master
 deployment 4
+ HEAD
 deploy to staging env
 echo good job.
 Tested and accepted
+HEAD
+deploy to staging env
+echo good job.
+Tested and accepted
+87895ff (tested and approved by client QA team)
+0a0542c (conflict 2 resolved)
